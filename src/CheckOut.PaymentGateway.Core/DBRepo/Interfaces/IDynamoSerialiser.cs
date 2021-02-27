a3321e2c2f41dff30cbc6a2d09e7f2f80b5c7c1b
Aditya A <aaditya29@gmail.com>
2021-02-27 23:13:42 +0000

    Add project files.

---
+++ src/CheckOut.PaymentGateway.Core/DBRepo/Interfaces/IDynamoSerialiser.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckOut.PaymentGateway.Core.DBRepo.Interfaces
+{
+    public interface IDynamoSerialiser<T>
+    {
+        Document ToDynamoDocument();
+
+        T ConvertToObject(Dictionary<string, AttributeValue> attributeMap);
+
+
+    }
+}
---
+++ src/CheckOut.PaymentGateway.Core/Enum/CurrencyCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckOut.PaymentGateway.Core.Enum
+{
+    public enum CurrencyCode
+    {
+        GBP = 1,
+        EUR = 2,
+        USD = 3,
+        INR = 4 
+    }
+}
